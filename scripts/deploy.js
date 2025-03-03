377a12df063833aacbcc758d67f917cdd8b5d34e
Kiarash <kkia2@myseneca.ca>
2025-03-02 23:01:44 -0500

    V1 Blockchain Deploy Contract

---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+const hre = require("hardhat");
+
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+    console.log("Deploying contract with account:", deployer.address);
+
+    // Contract details
+    const name = "TruMark";
+    const product = "Milk";
+    const creator = "Kiarash - Upayan";
+    const date = "3/2/2025";
+
+    // Deploy contract
+    const TruMark = await hre.ethers.getContractFactory("TruMark");
+    const truMark = await TruMark.deploy(name, product, creator, date);
+
+    await truMark.waitForDeployment();
+    console.log("Contract deployed to:", await truMark.getAddress());
+}
+
+// Run deployment
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
